54d8b67ee8225f6a8a9491e4589fa1152cbc99b6
Nicolas <medinamauvecin@gmail.com>
2021-03-28 21:04:43 -0300

    Agrega tarea incompleta de la clase 6
    Falta archivo calculos.js para terminar la tarea

--- clase-6/tarea-clase-6.js
+++ clase-6/tarea-clase-6.js
@@ -6,6 +6,45 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+document.querySelector('#boton-enviar').onclick = function(event){
+
+    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
+    const cantidadIntegrantes = Number($cantidadIntegrantes.value);
+
+    borrarElementosAnteriores();
+    crearElementos(cantidadIntegrantes);
+    return false;
+}
+
+function borrarElementosAnteriores(){
+    const $integrantesFamiliares = document.querySelectorAll('.integrante');
+    for (let i = 0; i < $integrantesFamiliares.length; i++) {
+        $integrantesFamiliares[i].remove();
+    }
+}
+
+function crearElementos(cantidadIntegrantes){
+    for (let i = 0; i < cantidadIntegrantes; i++) {
+        crearElemento(i);       
+    }
+}
+
+function crearElemento(indice){
+    const $div = document.createElement('div');
+    $div.className = 'integrante';
+
+    const $label = document.createElement('label');
+    $label.textContent = 'Ingresá la edad del integrante n° ' + (indice+1);
+
+    const $input = document.createElement('input');
+    $input.type = 'number';
+ 
+    $div.appendChild($label);
+    $div.appendChild($input);
+
+    const $contenedor = document.querySelector('#contenedor');
+    $contenedor.appendChild($div);
+}
 
 /*
 TAREA:
@@ -14,3 +53,5 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor sala
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
+
+
