d681a36c3853fe46bb8f346b8b8e79d5b06e2722
Nicolas <medinamauvecin@gmail.com>
2021-03-28 21:03:48 -0300

    Agrega tarea 4 de la clase 5

---
+++ clase-5/tarea4.js
@@ -0,0 +1,67 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+
+
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+
+const $valoresNumericos = document.querySelectorAll('li');
+
+document.querySelector('#calcular-promedio').onclick = function (){
+    let suma = 0;
+    for (let i = 0; i < $valoresNumericos.length; i++) {   
+        suma = suma + Number($valoresNumericos[i].textContent);
+    }
+    let promedio = suma / $valoresNumericos.length;
+    document.querySelector('em').textContent = `El promedio es ${promedio}`; 
+
+    return false;
+}
+
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+
+document.querySelector('#calcular-menor').onclick = function(){
+    let menorNumero = Number($valoresNumericos[0].textContent)
+
+    for (let i = 0; i < $valoresNumericos.length; i++) {
+        if (menorNumero > Number($valoresNumericos[i].textContent)){
+
+            menorNumero = Number($valoresNumericos[i].textContent);
+        } else {}
+
+    }
+    document.querySelector('em').textContent = `El número más chico es ${menorNumero}`;
+    return false;
+}
+
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+
+document.querySelector('#calcular-mayor').onclick = function(){
+    let mayorNumero = Number($valoresNumericos[0].textContent)
+
+    for (let i = 0; i < $valoresNumericos.length; i++) {
+        if (mayorNumero < Number($valoresNumericos[i].textContent)){
+
+            mayorNumero = Number($valoresNumericos[i].textContent);
+        } else {}
+
+    }
+    document.querySelector('em').textContent = `El número más grande es ${mayorNumero}`;
+    return false;
+}
+
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+// document.querySelector('#calcular-frecuente').onclick = function(){
+//     let sumador;
+//     for (let i = 0; i < $valoresNumericos.length; i++){
+//         for (let j = 1; j < $valoresNumericos.length; j++) {
+//             if (Number($valoresNumericos[i].textContent) === Number($valoresNumericos[j].textContent)){
+//                 console.log(Number($valoresNumericos[i].textContent))
+//                 sumador++; 
+//             }
+//         }
+//     }
+
+//     return false;
+// }
