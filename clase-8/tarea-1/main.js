3268f5c0334053740c91eb6dda6a23fbf3b648b4
Nicolas <medinamauvecin@gmail.com>
2021-04-05 21:44:06 -0300

    Agrega validaciones para Integrantes y Edades
    Se agregó las funciones para validar los campos y además como manejar cada tipo de error

--- clase-8/tarea-1/main.js
+++ clase-8/tarea-1/main.js
@@ -1,40 +1,89 @@
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+$form = document.querySelector('#formulario');
 
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
-*/
-
-document.querySelector('#boton-enviar').onclick = function(event){
-
-    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
-    const cantidadIntegrantes = Number($cantidadIntegrantes.value);
-
+document.querySelector('#boton-enviar').onclick = function(){
     borrarElementosAnteriores();
-    crearElementos(cantidadIntegrantes);
+    const $cantidadIntegrantes = document.querySelector("#cantidad-integrantes")
+    const cantidadIntegrantes = Number($cantidadIntegrantes.value)
+
+    const errorIntegrantes = validarCantidadIntegrantes(cantidadIntegrantes);
+
+    manejarErrorIntegrantes(errorIntegrantes, cantidadIntegrantes);
+    
     return false;
 }
 
-function borrarElementosAnteriores(){
-    const $integrantesFamiliares = document.querySelectorAll('.integrantes');
-    for (let i = 0; i < $integrantesFamiliares.length; i++) {
-        $integrantesFamiliares[i].remove();
+function validarCantidadIntegrantes(integrantes){
+    if (integrantes === 0){
+        return 'El campo integrantes no puede estar vacío.';
+    }
+    if (integrantes <= 1){
+        return 'El campo integrantes debe ser mayor a uno.';
+    } if (!/^[0-9]+$/.test(integrantes)){
+        return 'El campo integrantes debe tener un valor entero.';
+    }
+    return '';
+}
+
+function manejarErrorIntegrantes(errorIntegrantes, cantidadIntegrantes){
+    if (errorIntegrantes){
+        $form['cantidad-integrantes'].className = 'error';
+        reset();
+        return false;
+    } else {
+        $form['cantidad-integrantes'].className = '';
+        return crearElementos(cantidadIntegrantes);
+    }
+}
+
+document.querySelector('#boton-calcular').onclick = function(){
+    const $edadIntegrantes = document.querySelectorAll('.integrante');
+
+    const errorEdades = validarEdades($edadIntegrantes);
+    manejarErrorEdades(errorEdades);
+
+    let $resultados = document.querySelector('#resultados');
+    $resultados.innerText = `${manejarErrorEdades(errorEdades)}`;
+    return false;
+}
+
+function validarEdades(edades){
+    let a = [];
+    for (let i = 0; i < edades.length; i++) {
+        if (edades[i].value === ''){
+            a.push('El campo edad no puede estar vacío');
+        } else if (!/^[0-9]+$/.test(edades[i].value)){
+            a.push('El campo edad solo debe tener un valor entero.');
+        } else {
+            a.push('')
+        }
+    }
+    return a;   
+}
+
+function manejarErrorEdades(errorEdades){
+    const $edadIntegrantes = document.querySelectorAll('.integrante');
+    let errores = 0;
+    for (let i = 0; i < errorEdades.length; i++) {
+        if (errorEdades[i]){
+            document.querySelectorAll('.integrante')[i].className = 'integrante error';
+            errores++;
+        } else {
+            document.querySelectorAll('.integrante')[i].className = 'integrante';
+        }  
+    }
+
+    if (errores != 0){
+        return '';
+    } else{
+        return `El menor tiene ${calcularMenor($edadIntegrantes)}, el mayor ${calcularMayor($edadIntegrantes)} y el promedio de edad es de ${calcularPromedio($edadIntegrantes)}`;
     }
 }
 
 function crearElementos(cantidadIntegrantes){
-    if (cantidadIntegrantes > 1){
-        mostrarBotonReiniciar();
-        mostrarBotonCalcular();
-
-        for (let i = 0; i < cantidadIntegrantes; i++) {
-            crearElemento(i);       
-        }
-
-
-    } else{
-        reset();
+    mostrarBotonReiniciar();
+    mostrarBotonCalcular();
+    for (let i = 0; i < cantidadIntegrantes; i++) {
+        crearElemento(i);       
     }
 }
 
@@ -46,8 +95,7 @@ function crearElemento(indice){
     $label.textContent = 'Ingresá la edad del integrante n° ' + (indice+1);
 
     const $input = document.createElement('input');
-    $input.type = 'number';
-    $input.className = 'integrante'
+    $input.className = 'integrante';
  
     $div.appendChild($label);
     $div.appendChild($input);
@@ -56,19 +104,11 @@ function crearElemento(indice){
     $contenedor.appendChild($div);
 }
 
-document.querySelector('#boton-calcular').onclick = function(event){
-
-    const $edadIntegrantes = document.querySelectorAll('.integrante');
-
-    let $resultados = document.querySelector('#resultados');
-    $resultados.innerText = `El menor tiene ${calcularMenor($edadIntegrantes)}, el mayor ${calcularMayor($edadIntegrantes)} y el promedio de edad es de ${calcularPromedio($edadIntegrantes)}`;
-    mostrarResultados();
-    return false;
-}
-
-document.querySelector('#boton-reiniciar').onclick = function(){
-    reset();
-    return false;
+function borrarElementosAnteriores(){
+    const $integrantesFamiliares = document.querySelectorAll('.integrantes');
+    for (let i = 0; i < $integrantesFamiliares.length; i++) {
+        $integrantesFamiliares[i].remove();
+    }
 }
 
 function mostrarBotonCalcular(){
